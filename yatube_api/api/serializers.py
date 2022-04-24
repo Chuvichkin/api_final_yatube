a2951535628323f2df434b48d7bc87404e9eadcf
Сергей Чувычкин <chuvichkin@yandex.ru>
2022-04-24 22:47:39 +0300

    9 спринт

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,23 +1,56 @@
 from rest_framework import serializers
+from posts.models import Post, Group, Comment, Follow, User
 from rest_framework.relations import SlugRelatedField
-
-
-from posts.models import Comment, Post
+from rest_framework.validators import UniqueTogetherValidator
 
 
 class PostSerializer(serializers.ModelSerializer):
     author = SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
-        fields = '__all__'
+        fields = ('id', 'text', 'pub_date', 'author', 'image', 'group')
         model = Post
 
 
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('id', 'title', 'slug', 'description')
+        model = Group
+
+
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
-    )
+    author = SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
-        fields = '__all__'
+        fields = ('id', 'author', 'post', 'text', 'created')
         model = Comment
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = SlugRelatedField(
+        read_only=True,
+        default=serializers.CurrentUserDefault(),
+        slug_field='username'
+    )
+    following = SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
+
+    def validate_following(self, following):
+        if self.context.get('request').user == following:
+            raise serializers.ValidationError(
+                'Подписаться на самого себя нельзя!'
+            )
+        return following
+
+    class Meta:
+        fields = ('user', 'following')
+        model = Follow
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following'),
+                message=('Повторно подписаться нельзя!')
+            )
+        ]
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,16 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
+from django.urls import include, path
+from .views import PostViewSet, GroupViewSet, CommentViewSet, FollowViewSet
+
+router = DefaultRouter()
+router.register('v1/posts', PostViewSet)
+router.register('v1/groups', GroupViewSet)
+router.register('v1/follow', FollowViewSet, basename='Follows')
+router.register(r'v1/posts/(?P<post_id>\d+)/comments',
+                CommentViewSet, basename='comments')
 
 urlpatterns = [
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
+    path('', include(router.urls))
 ]
